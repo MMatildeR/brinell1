7695a613c87e7a2938b8421ffb03806d81f2abd8
MMatildeR <114346389+MMatildeR@users.noreply.github.com>
2023-07-22 20:52:37 -0300

    Delete GiftCardEpifaníaFacial copy directory

--- GiftCardEpifaníaFacial copy/main.js
+++
@@ -1,8 +0,0 @@
-// type="text/javascript"
- //document.addEventListener('mousemove', function(e){
-   // const body = document.querySelector('body');
-   // const bubbles = document.createElement('span');
-   // bubbles.style.left = e.offsetX+ 'px';
-   // bubbles.style.top = e.offsetY+ 'px';
-   // body.appendChild(bubbles);
-//})
